22198a4139d90e43517270df714a89b1e38d188b
chiranjeevsehgal <chiranjeevsehgal@gmail.com>
2024-01-13 19:06:09 +0530

    Major Changes

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,14 +6,23 @@ function Navbar() {
 
   return (
     <nav className="w-full bg-transparent text-neutral-900 px-5 md:px-10 flex items-center justify-between py-5 text-sm border-b-slate-200 border">
-      <h1 className="text-orange-500 text-2xl font-bold tracking-tight flex items-center gap-1">
+      {/* <h1 className="text-yellow-500 text-2xl font-bold tracking-tight flex items-center gap-1"> */}
+      
+      <h1 className="text-yellow-500 text-2xl font-bold tracking-tight flex items-center gap-1">
         Quiz<span className="text-neutral-900"> ify</span>
-        {/* <div className="w-2 h-2 bg-red-500 mt-2 ml-1 rounded-full" /> */}
       </h1>
+      {/* <h1 className="text-yellow-500 text-2xl font-bold tracking-tight flex items-center gap-1">
+      <Link
+            className="flex items-center gap-1"
+            to={"/"}
+          >
+            Quiz<span className="text-neutral-900">ify</span>
+          </Link>
+          </h1> */}
       <div className="space-x-5">
         {!auth?.email && (
           <Link
-            className="py-2 px-5 text-orange-500 font-semibold border rounded-full border-orange-500"
+            className="py-2 px-5 text-yellow-500 font-semibold border rounded-full border-yellow-500"
             to={"/login"}
           >
             Login
@@ -21,14 +30,14 @@ function Navbar() {
         )}
         {auth?.email ? (
           <button
-            className="py-2 transition px-5 text-neutral-50 font-semibold rounded-full bg-red-600"
+            className="py-2 transition px-5 text-neutral-50 font-semibold rounded-full bg-yellow-600"
             onClick={logoutUser}
           >
             Logout
           </button>
         ) : (
           <Link
-            className="py-2 transition px-5 text-neutral-50 font-semibold rounded-full bg-orange-500"
+            className="py-2 transition px-5 text-neutral-50 font-semibold rounded-full bg-yellow-500"
             to={"/register"}
           >
             Register
